de3db174cad746c48f99fb780f05ecf71cc6a3a1
Graham Christensen <graham@grahamc.com>
2018-03-24 21:17:26 -0400

    Move most of the drv in to downstream.nix which can be used by nixpkgs

---
+++ downstream.nix
@@ -0,0 +1,44 @@
+{ stdenv, lib, makeWrapper, dev_only_shellcheck ? null,
+fetchFromGitHub,
+
+fzf, coreutils, libxml2, libxslt, jing, findutils, gnugrep, gnused,
+docbook5
+}:
+stdenv.mkDerivation rec {
+  name = "xmloscopy-${version}";
+  version = "HEAD";
+
+  buildInputs = [
+    makeWrapper
+    dev_only_shellcheck
+  ];
+
+  spath = lib.makeBinPath [
+    fzf
+    coreutils
+    libxml2
+    libxslt
+    jing
+    findutils
+    gnugrep
+    gnused
+  ];
+
+  src = fetchFromGitHub {
+    owner = "grahamc";
+    repo = "xmloscopy";
+    rev = version;
+    sha256 = "0000000000000000000000000000000000000000000000000000";
+  };
+
+  installPhase = ''
+    type -P shellcheck && shellcheck ./xmloscopy
+    chmod +x ./xmloscopy
+    patchShebangs ./xmloscopy
+    mkdir -p $out/bin
+    cp ./xmloscopy $out/bin/
+    wrapProgram $out/bin/xmloscopy \
+      --set RNG "${docbook5}/xml/rng/docbook/docbook.rng" \
+      --set PATH "${spath}"
+  '';
+}
